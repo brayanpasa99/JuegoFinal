b2677e94f0f90893cc8f2ddc297abe0424b70268
brayanpasa99 <brayanpasa99@hotmail.com>
2019-02-17 19:17:29 -0500

    Update nuevamente

--- CampoBatalla.py
+++ CampoBatalla.py
@@ -4,6 +4,21 @@ from pygame.locals import *
 DIMENSIONES = (650, 500)
 COLOR_TEXTO = (243, 255, 0)
 
-class Batalla():
+class CampoBatalla():
 
-    def 
+    def castillos(self):
+        pygame.init()
+
+        jugando = True
+
+        ventana = pygame.display.set_mode(DIMENSIONES)
+        pygame.display.set_caption("Castillos")
+
+        while jugando:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+            pygame.display.update()
+
+
---
+++ main.py
@@ -0,0 +1,12 @@
+from CampoBatalla import CampoBatalla
+
+
+def main():
+
+    inicia = CampoBatalla()
+
+    inicia.castillos()
+
+
+if __name__ == "__main__":
+    main()
